2c2d7811fa2f9b24088dc8601205304e86120338
odijulio <odijulio@terpmail.umd.edu>
2025-02-12 19:07:44 -0500

    updated

--- Assets/Scenes/Lerp.cs
+++ Assets/Scenes/Lerp.cs
@@ -1,5 +1,7 @@
 // UMD IMDM290 
 // Instructor: Myungin Lee
+// Modified by Olivia DiJulio 
+// ASSIGNMENT 2 
     // [a <-----------> b]
     // Lerp : Linearly interpolates between two points. 
     // https://docs.unity3d.com/6000.0/Documentation/ScriptReference/Vector3.Lerp.html
@@ -17,9 +19,10 @@ public class Lerp : MonoBehaviour
     Vector3[] startPosition, endPosition;
     float lerpFraction; // Lerp point between 0~1
     float t;
-
-    // Start is called before the first frame update
-    void Start()
+    
+    // adding reference to material 
+    public Material newMaterial; 
+    void Start() 
     {
         // Assign proper types and sizes to the variables.
         spheres = new GameObject[numSphere];
@@ -32,35 +35,38 @@ public class Lerp : MonoBehaviour
             // Random start positions
             float r = 15f;
             startPosition[i] = new Vector3(r * Random.Range(-1f, 1f), r * Random.Range(-1f, 1f), r * Random.Range(-1f, 1f));        
-            // Heart shape end position
-            t = i* 2 * Mathf.PI / numSphere;
+            // equation given on GitHub 
+            t = i * 2 * Mathf.PI / numSphere;
             endPosition[i] = new Vector3( 
-                        5f*Mathf.Sqrt(2f) * Mathf.Sin(t) *  Mathf.Sin(t) *  Mathf.Sin(t),
-                        5f* (- Mathf.Cos(t) * Mathf.Cos(t) * Mathf.Cos(t) - Mathf.Cos(t) * Mathf.Cos(t) + 2 *Mathf.Cos(t)) + 3f,
-                        10f + Mathf.Sin(time));
+            5f * Mathf.Sqrt(2f) * Mathf.Sin(t) *  Mathf.Sin(t) *  Mathf.Sin(t),
+            5f * (- Mathf.Cos(t) * Mathf.Cos(t) * Mathf.Cos(t) - Mathf.Cos(t) * Mathf.Cos(t) + 2 * Mathf.Cos(t)) + 3f,
+            10f + Mathf.Sin(time));
         }
-        // Let there be spheres..
+        // Generating spheres 
         for (int i =0; i < numSphere; i++){
             float r = 10f; // radius of the circle
             // Draw primitive elements:
             // https://docs.unity3d.com/6000.0/Documentation/ScriptReference/GameObject.CreatePrimitive.html
-            spheres[i] = GameObject.CreatePrimitive(PrimitiveType.Sphere); 
+            // spheres[i] = GameObject.CreatePrimitive(PrimitiveType.Sphere); 
+
+            // change to cube 
+            spheres[i] = GameObject.CreatePrimitive(PrimitiveType.Cube); 
 
             // Position
             initPos[i] = startPosition[i];
             spheres[i].transform.position = initPos[i];
-
+            
             // Color
             // Get the renderer of the spheres and assign colors.
             Renderer sphereRenderer = spheres[i].GetComponent<Renderer>();
             // HSV color space: https://en.wikipedia.org/wiki/HSL_and_HSV
             float hue = (float)i / numSphere; // Hue cycles through 0 to 1
-            Color color = Color.HSVToRGB(hue, 1f, 1f); // Full saturation and brightness
+            Color color = Color.HSVToRGB(hue, 0.5f, 1f); // Full saturation and brightness
             sphereRenderer.material.color = color;
         }
     }
 
-    // Update is called once per frame
+    // lerps 
     void Update()
     {
         // Measure Time 
@@ -70,22 +76,41 @@ public class Lerp : MonoBehaviour
             // Lerp : Linearly interpolates between two points.
             // https://docs.unity3d.com/6000.0/Documentation/ScriptReference/Vector3.Lerp.html
             // Vector3.Lerp(startPosition, endPosition, lerpFraction)
-            
+
             // lerpFraction variable defines the point between startPosition and endPosition (0~1)
             // let it oscillate over time using sin function
-            lerpFraction = Mathf.Sin(time) * 0.5f + 0.5f;
+            
+            lerpFraction = Mathf.Sin(time) * 0.5f + 0.5f; 
 
             // Lerp logic. Update position       
-            t = i* 2 * Mathf.PI / numSphere;
+            t = i * 10 * Mathf.PI * numSphere;
             spheres[i].transform.position = Vector3.Lerp(startPosition[i], endPosition[i], lerpFraction);
+            
             // For now, start positions and end positions are fixed. But what if you change it over time?
             // startPosition[i]; endPosition[i];
 
             // Color Update over time
             Renderer sphereRenderer = spheres[i].GetComponent<Renderer>();
             float hue = (float)i / numSphere; // Hue cycles through 0 to 1
-            Color color = Color.HSVToRGB(Mathf.Abs(hue * Mathf.Sin(time)), Mathf.Cos(time), 2f + Mathf.Cos(time)); // Full saturation and brightness
+            // Saturation and Brightness 
+            Color color = Color.HSVToRGB(Mathf.Abs(hue * Mathf.Sin(time)), Mathf.Cos(time), 1f + Mathf.Cos(time));
             sphereRenderer.material.color = color;
+
+            // lerp position + jitter 
+            float jitter = Mathf.PerlinNoise(time + i, 0f); 
+            jitter = Mathf.Lerp(0.5f, 4f, jitter); 
+            spheres[i].transform.position = Vector3.Lerp(startPosition[i], endPosition[i], lerpFraction) * jitter; 
+
+            // changing size over time 
+            // lerp scale 
+            float minSize = 0.5f;
+            float maxSize = 3f;
+            float scaleValue = Mathf.Lerp(maxSize, minSize, lerpFraction); 
+
+            // apply lerp 
+            spheres[i].transform.localScale = new Vector3(scaleValue, scaleValue, scaleValue);
+
+
         }
     }
 }
